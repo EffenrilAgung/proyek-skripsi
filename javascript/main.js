64b8fc6292e3e8499b055bf737f1468421299efb
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2022-02-26 04:52:43 +0700

    fixing mobile

--- javascript/main.js
+++ javascript/main.js
@@ -2,59 +2,58 @@ let containerImage = document.querySelectorAll(
   "div.col.container-image-jumbotron"
 );
 
-window.addEventListener("resize", () => {
-  if (screen.width <= 800) {
-    for (let i = 0; i < containerImage.length; i++) {
-      containerImage[i].parentElement.removeChild(containerImage[i]);
+
+  window.addEventListener("load", () => {
+    if (screen.width <= 425) {
+      for (let i = 0; i < containerImage.length; i++) {
+        containerImage[i].parentElement.removeChild(containerImage[i]);
+      }
     }
-  }
-  if (screen.width <= 1200) {
-    let remove_col = document.querySelectorAll(
-      ".testimonial-main-4.col.col-md-4"
-    );
-    for (let i = 0; i < remove_col.length; i++) {
-      let a = remove_col[i];
-      a.classList.remove("col-md-4");
-      a.classList.add("col-md-8");
-    }
-  }
-  if (screen.width <= 1024) {
-    let remove_col_md_4 = document.querySelectorAll(
-      ".col.col-md-3.container-content-des-main-5"
-    );
-    let remove_offset = document.querySelector(".col.col-md-4.offset-1");
-    remove_offset.classList.remove("offset-1");
-    for (let i = 0; i < remove_col_md_4.length; i++) {
-      let b = remove_col_md_4[i];
-      b.classList.remove("col-md-3");
-      b.classList.add("col-md-4");
-    }
-  }
-    if (screen.width <= 800) {
-      let container_content_title = document.querySelector(
-        ".container-content-title"
-      );
-      let add_content = [
-        "d-flex",
-        "flex-column",
-        "justify-content-evenly",
-        "align-items-center",
-      ];
-     for (const arr_add_content of add_content ) {
-        container_content_title.classList.add(arr_add_content)
-     }
-    }
-});
+
+      if (screen.width <= 1200) {
+        let remove_col = document.querySelectorAll(
+          ".testimonial-main-4.col.col-md-4"
+        );
+        for (let i = 0; i < remove_col.length; i++) {
+          let a = remove_col[i];
+          a.classList.remove("col-md-4");
+          a.classList.add("col-md-8");
+        }
+      }
+      if (screen.width <= 1024) {
+        let remove_col_md_4 = document.querySelectorAll(
+          ".col.col-md-3.container-content-des-main-5"
+        );
+        let remove_offset = document.querySelector(".col.col-md-4.offset-1");
+        remove_offset.classList.remove("offset-1");
+        for (let i = 0; i < remove_col_md_4.length; i++) {
+          let b = remove_col_md_4[i];
+          b.classList.remove("col-md-3");
+          b.classList.add("col-md-4");
+        }
+      }
+        if (screen.width <= 425) {
+          let container_content_title = document.querySelector(
+            ".container-content-title"
+          );
+          let add_content = [
+            "d-flex",
+            "flex-column",
+            "justify-content-evenly",
+            "align-items-center",
+          ];
+         for (const arr_add_content of add_content ) {
+            container_content_title.classList.add(arr_add_content)
+         }
+        }
+  });
 
 // scroll
 
-window.onscroll = function () {
-  navbarEffect();
-};
 function navbarEffect() {
   let backgroundNav = document.querySelector(
     "nav.navbar.navbar-expand-lg.px-5.fixed-top"
-  );
+    );
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
     backgroundNav.style =
       "background: linear-gradient(90.19deg, rgba(10, 199, 117, 0.37) 0.55%, rgba(60, 88, 233, 0.38) 99.57%)";
@@ -81,13 +80,16 @@ function scrollinghome() {
   elementId.scrollIntoView({ behavior: "smooth" });
 }
 
+window.onscroll = function () {
+  navbarEffect();
+};
 // percen
 let school = document.querySelector("h5.number-main-5.school");
 let student = document.querySelector("h5.number-main-5.student");
 let containerMain5 = document.querySelector(".container-content-des-main-5");
 let count = 0;
 let count2 = 0;
-containerMain5.addEventListener("click", () => {
+window.addEventListener("load", () => {
   let intervalNumber = setInterval(animasi, 20);
   let intervalNumber2 = setInterval(animasi2, 1);
   function animasi() {
@@ -107,3 +109,8 @@ containerMain5.addEventListener("click", () => {
     }
   }
 });
+
+// let aa = document.querySelector('.container-fluid.main-5')
+// let bb = []
+// bb.push(window.scrollTop)
+// console.log(aa.scrollY)
